43b0a4716ecba9402d618c5088de6c6a12735286
Alina <ahsieh74@gmail.com>
2023-08-21 16:27:20 +0800

    20230821

---
+++ s20230821/cors_backend.php
@@ -0,0 +1,33 @@
+<?php
+
+// header('Access-Control-Allow-Origin: http://blue-front.com:5501');
+
+// ajax html s20230821_05_ajax_kai.html
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept');
+
+function dd($data)
+{
+    echo "<pre>";
+    print_r($data);
+    echo "<pre>";
+}
+
+
+$data = $_GET;
+// dd($data);
+
+$data['sum'] =  ($data['num1'] + $data['num2']) * $data['num3'];
+
+
+// $data = [
+//     's1' => 'amy',
+//     's2' => 'bob',
+//     's3' => 'cat'
+// ];
+
+// dd($data);
+
+echo json_encode($data);
